4018accbf34b0c501cae10c8538d27fa90f663cb
hecter_wu <hecter_wu@transcend.com.tw>
2017-04-27 14:25:17 +0800

    pass test case testSameHeight by using sorted and itemgetter

--- Queue_Reconstruction_by_Height.py
+++ Queue_Reconstruction_by_Height.py
@@ -1,14 +1,19 @@
 import unittest
+from operator import itemgetter
 
 class unitest(unittest.TestCase):
     def testNull(self):
         people = []
         self.assertEqual(Solution().reconstructQueue(people),people);
-
+    def testSameHeight(self):
+        people = [[5,1],[5,0],[5,2]]
+        ans = [[5,0],[5,1],[5,2]]
+        self.assertEqual(Solution().reconstructQueue(people),ans);
 class Solution():
     def reconstructQueue(self, people):
         if(people == []):
             return people
+        return sorted(people, key=itemgetter(1))
 
 if __name__ == '__main__':
     unittest.main()
