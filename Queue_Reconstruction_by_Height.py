0109675f9061c4f1929a539075b6ad868da1094c
hecter_wu <hecter_wu@transcend.com.tw>
2017-04-28 17:40:09 +0800

    refactor : using lambda instead

--- Queue_Reconstruction_by_Height.py
+++ Queue_Reconstruction_by_Height.py
@@ -1,6 +1,4 @@
 import unittest
-from operator import itemgetter
-from collections import defaultdict
 
 class unitest(unittest.TestCase):
     def testNull(self):
@@ -15,12 +13,10 @@ class unitest(unittest.TestCase):
         ans = [[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
         self.assertEqual(Solution().reconstructQueue(people),ans);
 class Solution():
-    def my_key(self,people):
-        return -people[0],people[1]
     def reconstructQueue(self, people):
         if(people == []):
             return people
-        people.sort(key=Solution().my_key)
+        people.sort(key=lambda a: (-a[0], a[1]))
         answer = []
         for person in people:
             answer.insert(person[1],person)
